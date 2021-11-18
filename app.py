6e782ab2a9e7d4080b83fe812b2996f91eb4a848
neopanis <88994765+neopanis@users.noreply.github.com>
2021-11-18 13:26:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,13 +151,16 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
           replyObj = TextSendMessage(text="ดีด้วย") 
+          line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "กินข้าวยังคะ") :
           replyObj = TextSendMessage(text="กินแล้วค่ะ") 
+          line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "มีแฟนยัง") :
           replyObj = TextSendMessage(text="สภาพ โสดจ้า")
+          line_bot_api.reply_message(rtoken, replyObj)
         else :
             replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
